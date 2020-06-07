368e0a2df9ffdbe285f54866be1c42208d64aab5
JohnCoene <jcoenep@gmail.com>
2020-06-07 14:43:18 +0200

    corrections

--- code/jbox/inst/assets/custom.js
+++ code/jbox/inst/assets/custom.js
@@ -2,7 +2,7 @@
 Shiny.addCustomMessageHandler(type = 'send-alert', function(message) {
   message.notice.onClose = function(){
     console.log("close");
-    Shiny.setInputValue(message.id + '_alert_close', true);
+    Shiny.setInputValue(message.id + '_alert_close', true, {priority: 'event'});
   }
   new jBox('Notice', message.notice);
 });
