0c43d3ea46080f3eb9e96c9fb2014fcd92ca5369
JohnCoene <jcoenep@gmail.com>
2020-06-07 10:44:13 +0200

    correct full monty

--- _build.sh
+++ _build.sh
@@ -1,6 +1,10 @@
 #!/bin/sh
 
+echo "Cleaning book"
 Rscript -e "bookdown::clean_book()"
+echo "Creating HTML"
 Rscript -e "bookdown::render_book('.', 'bookdown::gitbook')"
+echo "Creating PDF"
 Rscript -e "bookdown::render_book('.', 'bookdown::pdf_book')"
+echo "Creating epub"
 Rscript -e "bookdown::render_book('.', 'bookdown::epub_book')"
