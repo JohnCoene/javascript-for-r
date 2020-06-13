d5c5276ff912d50c5589c96b0d077c3b661c99ad
JohnCoene <jcoenep@gmail.com>
2020-06-13 23:30:51 +0200

    update section

--- README.md
+++ README.md
@@ -3,5 +3,9 @@
 To build the book.
 
 ```bash
+# install packages
+./_install.sh
+
+# build book
 ./_build-sh
 ```
--- _build.sh
+++ _build.sh
@@ -1,8 +1,10 @@
 #!/bin/sh
 
-# render book
+# clean
 echo "\e[31mCleaning book\033[0m"
 Rscript -e "bookdown::clean_book()"
+
+# render book
 echo "\e[33mCreating HTML\033[0m"
 Rscript -e "bookdown::render_book('.', 'bookdown::gitbook')"
 echo "\e[33mCreating PDF\033[0m"
