ced44279def4777710200365b3680ca6b88490a7
JohnCoene <jcoenep@gmail.com>
2020-07-01 22:20:13 +0200

    tips & tricks added for shiny

---
+++ code/table-button/app.R
@@ -0,0 +1,23 @@
+library(DT)
+library(shiny)
+
+ui <- fluidPage(
+  DTOutput("table"),
+  strong("Clicked Model:"),
+  verbatimTextOutput("model")
+)
+
+server <- function(input, output) {
+  output$table <- renderDT({
+    onclick <- paste0("Shiny.setInputValue('click', '", rownames(mtcars), "')")
+    button <- paste0("<a class='btn btn-primary' onClick=\"", onclick, "\">Click me</a>")
+    mtcars$button <- button
+    datatable(mtcars, escape = FALSE, selection = "none", rownames = FALSE)
+  })
+
+  output$model <- renderPrint({
+    print(input$click)
+  })
+}
+
+shinyApp(ui, server)
