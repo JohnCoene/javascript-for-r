5d2cf07e45425dd090d59bcdf08fd16b653cbb38
JohnCoene <jcoenep@gmail.com>
2020-07-04 21:43:04 +0200

    corrects and improves events example

--- code/events/app.R
+++ code/events/app.R
@@ -3,9 +3,10 @@ library(shiny)
 shiny::addResourcePath("www", "www")
 
 ui <- fluidPage(
-  tags$link(href = "www/style.css", rel = "stylesheet"),
-  tags$script(src = "www/script.js"),
-  h1("What color am I?", id = "color"),
+  tags$head(
+    tags$link(href = "www/style.css", rel = "stylesheet"),
+    tags$script(src = "www/script.js")
+  ),
   tags$img(src = "www/typing.gif", id = "loading"),
   plotOutput("plot"),
   actionButton("render", "render")
@@ -14,7 +15,7 @@ ui <- fluidPage(
 server <- function(input, output, session) {
   output$plot <- renderPlot({
     input$render
-    Sys.sleep(2)
+    Sys.sleep(10)
     plot(cars)
   })
 }
