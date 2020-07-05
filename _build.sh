1dcc61d071339c0fa3bc52c0e9d2baffe69cf295
JohnCoene <jcoenep@gmail.com>
2020-07-05 12:56:17 +0200

    correct rsup example

--- _build.sh
+++ _build.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 
 # clean
-echo "\e[31mCleaning book\033[0m"
-Rscript -e "bookdown::clean_book()"
+# echo "\e[31mCleaning book\033[0m"
+# Rscript -e "bookdown::clean_book()"
 
 # render book
 echo "\e[33mCreating HTML\033[0m"
