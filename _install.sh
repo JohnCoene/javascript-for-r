1f5cfe7390518211d66f329c9a2bd19ee7341003
JohnCoene <jcoenep@gmail.com>
2020-07-17 15:28:06 +0200

    corrections + img improvements + jQuery event init

--- _install.sh
+++ _install.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 
+Rscript -e "setwd('./code/test');devtools::install();"
 Rscript -e "setwd('./code/gio');devtools::install();"
 Rscript -e "setwd('./code/jbox');devtools::install();"
 Rscript -e "setwd('./code/ms');devtools::install();"
