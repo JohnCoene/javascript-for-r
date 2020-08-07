f11ab511af5151b344b0acb4139b5388cbd4fc03
JohnCoene <jcoenep@gmail.com>
2020-08-07 21:21:12 +0200

    restructure and redo 1-01 + index

--- _build.sh
+++ _build.sh
@@ -2,7 +2,7 @@
 
 # clean
 # echo "\e[31mCleaning book\033[0m"
-# Rscript -e "bookdown::clean_book()"
+Rscript -e "bookdown::clean_book()"
 
 # render book
 echo "\e[33mCreating HTML\033[0m"
--- code/test/R/function.R
+++
@@ -1,7 +0,0 @@
-#' @export
-print_file <- function(){
-  file <- system.file("javascript.js", package = "test")
-  cli::cli_alert_info("JavaScript code below.")
-  content <- readLines(file)
-  print(content)
-}
